1cdf05dbf35856f4b19fe041730a09c9b6d470f7
jmihc <jcornyn@gmail.com>
2022-11-11 23:05:00 -0800

    add first edit

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,24 +3,7 @@
 function Header() {
     return (
       <div className="flex justify-between my-8 items-center">
-        <h1 className="font-medium text-xl">Next.js Blog</h1>
-        <div>
-          <a
-            href="https://twitter.com/MarkSShenouda"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-4"
-          >
-            Twitter
-          </a>
-          <a
-            href="https://github.com/markshenouda"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-        </div>
+        <h1 className="font-medium text-xl">Welcome</h1>
       </div>
     );
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import getPosts from "../helpers/getPosts";
 export default function Home({ posts }) {
   return (
     <div>
-      <h1 className="mt-24 mb-12 font-bold text-3xl">Latest Posts</h1>
+      <h2 className="mt-24 mb-12 font-bold text-3xl">Starting up soon</h2>
       {posts.map((post) => (
         <PostCard
           key={post.slug}
